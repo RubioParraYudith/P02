a11f8a33dc32f914a033f90554e8d26538c9bdc6
Yudith Rubio <yudiht17@gmail.com>
2020-05-06 19:37:51 -0700

    actualizacion P02

---
+++ ControlInventario/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int ProductCode { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+
+        public int SupplirId { get; set; }
+        [ForeignKey("SupplirId")]
+
+        public Supplier Supplier { get; set; }
+    }
+}
